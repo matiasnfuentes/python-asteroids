dc2d0a19124a18c057b5368c67534069f7d04c6f
matiasnfuentes <matiasnfuentes@gmail.com>
2025-07-05 22:30:18 +0000

    add clock ticks

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
 
 def game_loop():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,6 +24,7 @@ def game_loop():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
